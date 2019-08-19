06b95315f5d795806f63fed14f9d22eac57fbc2b
hoangngth <42316439+hoangngth@users.noreply.github.com>
2019-08-19 13:13:53 +0700

    update script

--- Implementation/test1.py
+++ Implementation/test1.py
@@ -2,19 +2,23 @@ import os
 import numpy as np
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Embedding, Flatten, Dense
 
 utterances = []
 texts = []
 labels = []
 
 # DATA PRE-PROCESSING
-with open(os.getcwd()+'/Dataset/4_labels_emo.txt', encoding='utf-8', errors='ignore') as f:
+emotion_dataset_dir = os.getcwd()+'/Dataset/4_labels_emo.txt'
+with open(emotion_dataset_dir, encoding='utf-8', errors='ignore') as f:
     raw_dataset = f.readlines()
 raw_dataset.pop(0)
+f.close()
 
 for row in raw_dataset:
     temp_splitted = row.split('\t')
-    labels.append(temp_splitted[4])
+    labels.append(temp_splitted[4].strip('\n'))
     temp_splitted.pop(4)
     temp_splitted.pop(0)
     utterances.append(temp_splitted)
@@ -30,4 +34,55 @@ sequences = tokenizer.texts_to_sequences(texts) # Turn text into sequence of num
 
 maxlen = 100 # Make all sequences 100 words long
 data = pad_sequences(sequences, maxlen=maxlen)
-print(data.shape) # We have 25K, 100 word sequences now
+print(data.shape) # We have 5509, 100 word sequences now
+
+labels = np.asarray(labels)
+print(labels.shape)
+
+# Determine train and validation data
+training_samples_ratio = 0.8
+training_samples = round(training_samples_ratio * data.shape[0])
+validation_samples = data.shape[0] - training_samples
+
+# Split data
+x_train = data[:training_samples]
+y_train = labels[:training_samples]
+x_val = data[training_samples: training_samples + validation_samples]
+y_val = labels[training_samples: training_samples + validation_samples]
+
+# Load word embedding, process WE file
+emotional_glove_dir = os.getcwd() + '/Word_Embedding/em-glove.6B.300d-20epoch.txt'
+embedding_index = dict()
+print('Converting into dictionary of vectorized words...')
+f = open(emotional_glove_dir, encoding='utf-8', errors='ignore')
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embedding_index[word] = coefs
+f.close()
+print('Done.')
+
+# Create a weight matrix for words in training
+vocab_size = len(tokenizer.word_index)+1
+
+embedding_dim = 300
+embedding_matrix = np.zeros((vocab_size, embedding_dim))
+for word, i in tokenizer.word_index.items():
+    embedding_vector = embedding_index.get(word)
+    if embedding_vector is not None:
+        embedding_matrix[i] = embedding_vector
+    else:
+        embedding_matrix[i] = np.zeros(embedding_dim, )
+
+# Model
+model = Sequential()
+model.add(Embedding(vocab_size, embedding_dim, input_length=maxlen, weights = [embedding_matrix], trainable = False))
+model.add(Flatten())
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.summary()
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['acc'])
